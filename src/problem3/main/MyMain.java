cb5f53d1e4a7981ab68db0f525e56954c7e2282a
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 20:56:31 +0530

    creating main methods and inserting values and inserting them

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,6 +6,18 @@
  */
 package problem3.main;
 // executable class
+
+import problem3.myqueue.MyPriorityQueue;
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Yash", 59);
+        obj.insert("Mohit", 19);
+        obj.insert("Muskan", 20);
+        obj.insert("Aishwarya", 2);
+        obj.insert("Uddeshya", 53);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
