5203dd7da731bab0786d08214457713b6cb109ff
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 20:52:51 +0530

    declearing getter and setter methods

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,6 +6,25 @@
  */
 package problem3.node;
 
-public class Node {
+import problem5.student.Student;
 
+public class Node {
+    Student obj;
+    Node next;
+
+    public Student getObj() {
+        return obj;
+    }
+
+    public void setObj(Student obj) {
+        this.obj = obj;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
