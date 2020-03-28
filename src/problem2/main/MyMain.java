4238b312ea7bcc90b8badae4bcb725049dd9d118
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 20:25:48 +0530

    creating main method and inserting value

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(20);
+        obj.insert(22);
+        obj.insert(19);
+        obj.insert(15);
+        obj.insert(25);
+        obj.insert(29);
+        obj.insert(5);
+        System.out.print("Left Child nodes are: ");
+        obj.printLeftNode(obj.root);
+        System.out.println();
+        System.out.println("No of nodes that dosn't have a left child node are: " + obj.getCount());
+    }
 }
