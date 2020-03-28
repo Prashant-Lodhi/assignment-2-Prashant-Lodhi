204668088f8e06169c14cd72c16ea6c7355cde30
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 20:37:49 +0530

    creating getter and setter methods

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,6 +7,24 @@
 package problem5.node;
 // to define node properties
 
+import problem5.student.Student;
 public class Node {
+    Student student;
+    Node next;
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
