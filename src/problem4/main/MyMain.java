1714b17aae73afdb2315a9564a0f30fcdcc1edb8
abhishektiwari <abhishek.tiwari_cs18@gla.ac.in>
2020-03-28 20:51:15 +0530

    creating main method and inserting value and display them

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -8,8 +8,21 @@ package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
 public class MyMain {
     public static void main(String[] args) {
-
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(12);
+        obj.insert(18);
+        obj.insert(19);
+        obj.insert(17);
+        obj.insert(27);
+        obj.insert(29);
+        MyQueue obj1 = new MyQueue();
+        obj1.preOrderSucc(obj.root);
+        System.out.print("PreOrder Successor of all the nodes are :");
+        obj1.dispPreOrderSucc();
     }
 }
